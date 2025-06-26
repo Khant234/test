923b39c8bddab22f7a098de3664ff6794f14e7a6
Khant234 <khantzinwin1000@gmail.com>
2025-06-27 00:41:50 +0630

    fix: Apply fixes from RepoMedic

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         </a>
         get="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </get=>
+        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
